42cd32966be4091d25e07039cfc2dd8a181341c2
jkhanjan <jhakhanjan62@gmail.com>
2026-04-07 22:53:47 +0530

    fornend protection added

--- frontend/src/hooks/ProtectedRoute.jsx
+++ frontend/src/hooks/ProtectedRoute.jsx
@@ -7,5 +7,18 @@ export default function ProtectedRoute({ children }) {
   if (!token) {
     return <Navigate to="/auth/login" replace />;
   }
+
+  try {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    const isExpired = payload.exp * 1000 < Date.now();
+
+    if (isExpired) {
+      alert("Session expired. Please login again.");
+      return <Navigate to="/auth/login" replace />;
+    }
+  } catch (error) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
   return children;
 }
