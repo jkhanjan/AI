d8daa8498571286ecbb928ce5c817b01e95c307b
jkhanjan <jhakhanjan62@gmail.com>
2026-06-03 21:10:38 +0530

    CORS issue

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const connectDB = require('./src/config/db');
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL ,'http://localhost:5173'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
