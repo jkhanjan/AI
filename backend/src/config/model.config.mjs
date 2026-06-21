42dba4a343c228bcf374a8ae620677cf855ce3d9
jkhanjan <jhakhanjan62@gmail.com>
2026-06-21 08:07:12 +0530

    model changes

--- backend/src/config/model.config.mjs
+++ backend/src/config/model.config.mjs
@@ -1,5 +1,12 @@
 export const MODELS = {
-  FAST: "llama-3.1-8b-instant",
-  SMART: "llama-3.3-70b-versatile",
-  REASONING: "openai/gpt-oss-20b"
+  "llama-3.1-8b-instant": {
+    id: "llama-3.1-8b-instant",
+    for: ["simple"],
+    provider: "groq"
+  },
+  "llama-3.3-70b-versatile": {
+    id: "llama-3.3-70b-versatile",
+    for: ["complex", "pdf", "code"],
+    provider: "groq"
+  }
 };
